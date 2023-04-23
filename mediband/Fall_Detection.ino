acae3554c6ed48b218263d4cd71de18b770b7c94
Sanidu Anupama <dlsanupama@students.nsbm.ac.lk>
2023-04-23 18:39:59 +0530

    added fall detection

--- mediband/Fall_Detection.ino
+++ mediband/Fall_Detection.ino
@@ -1,1 +1,100 @@
+void Detect_Fall(){
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
 
+  float accel_mag=sqrt(pow(a.acceleration.x,2)+pow(a.acceleration.y,2)+pow(a.acceleration.z,2));
+
+  float gyro_mag=sqrt(pow(g.gyro.x,2)+pow(g.gyro.y,2)+pow(g.gyro.z,2));
+
+  if ((accel_mag > THRESHOLD_ACCEL)&&(gyro_mag > THRESHOLD_GYRO)) {
+    // check if the magnitude of the gyro vector exceeds the threshold
+      Serial.println("Fall detected!");
+      // do something when a fall is detected 
+  }
+  else{
+    Serial.println("No Fall detected!");
+  }
+
+  Serial.println("");
+  delay(500);
+}
+
+
+void Setup_FallSensor(){
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  // delay(100);
+}
--- mediband/mediband.ino
+++ mediband/mediband.ino
@@ -1,11 +1,16 @@
 #define buzzerPin D4 // Set the pin for the buzzer
+#define THRESHOLD_ACCEL 11 // accelerometer threshold for detecting a fall
+#define THRESHOLD_GYRO 11 // gyro threshold for detecting a fall
 
-// Heart Rate Monitor - Start
 #include <Wire.h>
 #include "MAX30105.h"
 #include "heartRate.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
 MAX30105 particleSensor;
+Adafruit_MPU6050 mpu;
 
 const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
@@ -14,17 +19,16 @@ long lastBeat = 0; //Time at which the last beat occurred
 
 float beatsPerMinute;
 int beatAvg;
-// Heart Rate Monitor - End
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
   Serial.begin(115200);
 
-  //Heart Rate Monitor - Start
-  Setup_Hbeat_Sensor();
-  //Heart Rate Monitor - End
+  // Setup_Hbeat_Sensor();
+  Setup_FallSensor();
 }
 
 void loop() {
-  Siren_Buzz();
+  // Start Detecting Fall
+  Detect_Fall();
 }
