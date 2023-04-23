b7af5f849fbc2d03ecd5cc85d95810e8cf916009
NethunRanasinghe <77114870+NethunRanasinghe@users.noreply.github.com>
2023-04-23 18:04:56 +0530

    Hbeat setup moved

--- mediband/Hbeat.ino
+++ mediband/Hbeat.ino
@@ -34,3 +34,19 @@ void Get_Hbeat(){
 
   Serial.println();
   }
+
+
+void Setup_Hbeat_Sensor(){
+    Serial.println("Initializing...");
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  }
