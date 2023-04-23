6933271fbdcfc85bbffdf2b9a95e92f81416cae7
NethunRanasinghe <77114870+NethunRanasinghe@users.noreply.github.com>
2023-04-23 17:59:50 +0530

    Added Hbeat Temp Code

--- mediband/mediband.ino
+++ mediband/mediband.ino
@@ -1,12 +1,43 @@
 #define buzzerPin D4 // Set the pin for the buzzer
 
+// Heart Rate Monitor - Start
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
+// Heart Rate Monitor - End
+
 void setup() {
   pinMode(buzzerPin, OUTPUT);
+  Serial.begin(115200);
 
-  Serial.begin(9600);
+  //Heart Rate Monitor - Start
+  Serial.println("Initializing...");
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+
+  //Heart Rate Monitor - End
 }
 
 void loop() {
   Siren_Buzz();
-
 }
